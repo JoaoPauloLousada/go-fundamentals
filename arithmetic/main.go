28d42014b3b6393b009c5c0efdc35ba7c1c1fda2
Joao Lousada <joao.lousada.ext@makeo.com>
2023-07-26 22:31:24 +0200

    arithmetic operations

---
+++ arithmetic/main.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	a, b := 10, 5
+	c := a + b // addition
+	fmt.Println(c)
+	c = a - b // subtraction
+	fmt.Println(c)
+	c = a * b //multiplication
+	fmt.Println(c)
+	c = a / b // division
+	fmt.Println(c)
+	c = a % 3 // modulus
+	fmt.Println(c)
+	d := 7.0 / 2.0 // decimal results
+	fmt.Println(d)
+}
